ac4c5696d4fcaf969758a3b47da3a4825d9031e3
Lydia <lydiajarvistx@gmail.com>
2024-11-01 18:36:32 -0500

    adding api

---
+++ index.html/main.js
@@ -0,0 +1,33 @@
+async function displayData() {
+    const giphyApiKey = "Zy3h56fvBHGzyNS61BYzn7rqnjxy92uY";
+    const searchValue = document.getElementById("searchInput").value;
+    const limit = 10;
+    const offset = 0;
+    const url = `http://api.giphy.com/v1/gifs/search?q=${searchValue}&api_key=${giphyApiKey}&limit=${limit}&offset=${offset}`;
+    try {
+      const response = await fetch(url);
+      console.log (response)
+      if (!response.ok) throw new Error("Network response was not ok.");
+      
+      const data = await response.json();
+      console.log (data)
+      const sectionImage = document.getElementById("section_image");
+      console.log (sectionImage)
+      sectionImage.innerHTML = ""; // Clear previous results if any
+
+
+      data.data.forEach(element => {
+      console.log (element)
+
+        const newImage = document.createElement("img");
+        newImage.src = element.images.original.url;
+        newImage.className = "giphy_img";
+        sectionImage.appendChild(newImage);
+      });
+
+
+      
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  }
